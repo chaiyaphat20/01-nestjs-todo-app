85467da1a668dde0b7d0044b42d97dda291d299c
chaiyaphat <chaiyaphat.b5922314@gmail.com>
2024-06-11 14:08:10 +0700

    feat: add model

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/api/v1');
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('TodoApp')
@@ -13,6 +16,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
